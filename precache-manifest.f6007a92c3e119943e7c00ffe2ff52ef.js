2d01041adfd55838f83cc70ccef535f1d3f30c2a
Danny Weinberg <FuegoFro@gmail.com>
2021-01-13 15:53:31 -0800

    deploy

---
+++ precache-manifest.f6007a92c3e119943e7c00ffe2ff52ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea79e861d5be60d11179",
+    "url": "/GithubReView/css/app.6a616a62.css"
+  },
+  {
+    "revision": "ea79e861d5be60d11179",
+    "url": "/GithubReView/js/app.e334dd7e.js"
+  },
+  {
+    "revision": "caded858e301da5bcecb",
+    "url": "/GithubReView/js/chunk-vendors.0a98d0d8.js"
+  },
+  {
+    "revision": "157d84f279f4098d9983d2e83af22470",
+    "url": "/GithubReView/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/GithubReView/robots.txt"
+  }
+];
